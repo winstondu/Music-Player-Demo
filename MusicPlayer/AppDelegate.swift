f27535a0a1358457bd9d0445ad193c36e5c28a38
Winston Du <wenhaodu694@gmail.com>
2025-11-05 02:24:30 -0500

    Make app landscape

---
+++ MusicPlayer/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MusicPlayer
+//
+//  Created by Winston Du on 11/5/25.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        return .landscape
+    }
+}
