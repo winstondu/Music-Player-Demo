6ff72af1b56ba26624df3b0399c42855600dd304
Winston Du <wenhaodu694@gmail.com>
2025-11-09 22:27:30 -0500

    Fix music default

--- MusicPlayer/Models/Constants+MusicTrack.swift
+++ MusicPlayer/Models/Constants+MusicTrack.swift
@@ -52,63 +52,63 @@ let taylorSwift1989Tracks: [MusicTrack] = [
         id: "1440936016",
         name: "Shake It Off",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/3e/1c/4c/3e1c4c8f-d870-de09-c793-1d3256b5fc40/mzaf_17159063720592691370.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview126/v4/5e/10/7c/5e107c9b-5fe8-581a-5ea4-717903781d94/mzaf_13937309032449903025.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 219209
     ),
     MusicTrack(
-        id: "1440936017",
+        id: "1440936021",
         name: "I Wish You Would",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/15/2d/a4/152da416-3fd4-6f6b-2b4d-bb5c8d3dda01/mzaf_4042333788870806308.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/df/cb/56/dfcb566d-ef33-c5df-04b5-67d2cc73a373/mzaf_16852456055802287441.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 207435
     ),
     MusicTrack(
-        id: "1440936018",
+        id: "1440936028",
         name: "Bad Blood",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/90/4b/1d/904b1de8-4b16-8356-8e42-f48aac7a4456/mzaf_2653845612453929960.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/80/e6/a8/80e6a803-1117-cd3f-48da-a3e6799eea3c/mzaf_10174351058545549372.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 211933
     ),
     MusicTrack(
-        id: "1440936019",
+        id: "1440936036",
         name: "Wildest Dreams",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/7b/b1/6e/7bb16ee7-2e8e-ff03-a470-f67b4f8e96c5/mzaf_10926205883691838192.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/84/fa/ad/84faad60-0a35-ddaf-ceb5-2ff09e0b0e5a/mzaf_4333199558904738055.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 220433
     ),
     MusicTrack(
-        id: "1440936020",
+        id: "1440936195",
         name: "How You Get the Girl",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/46/2b/e6/462be67c-d4bd-e4d4-5f7f-e00e75feeca9/mzaf_1353506682882253967.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/ba/83/49/ba834950-82cd-b435-0760-b1d24e9eafa1/mzaf_6100216268001075273.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 247533
     ),
     MusicTrack(
-        id: "1440936021",
+        id: "1440936201",
         name: "This Love",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/32/56/9c/32569c2a-8f23-ea96-c5c9-fd48d3668ee1/mzaf_8849668089374686524.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/b5/bc/2d/b5bc2d4d-fdd2-f803-d30f-5badd917b82c/mzaf_6131896205773781695.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 250100
     ),
     MusicTrack(
-        id: "1440936022",
+        id: "1440936203",
         name: "I Know Places",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/da/f1/02/daf10256-ea76-b57b-e835-43e65e3e7e09/mzaf_10382368086651506026.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/5d/26/32/5d2632fc-68fc-68be-9b11-1b47f92faed2/mzaf_12306884528528576424.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 195700
     ),
     MusicTrack(
-        id: "1440936023",
+        id: "1440936206",
         name: "Clean",
         artist: "Taylor Swift",
-        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/b5/39/c5/b539c5d1-d13c-15e1-5b9a-34d0e8c77dc2/mzaf_16968155372253829434.plus.aac.p.m4a"),
+        previewURL: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/66/4b/ac/664bac15-9db3-dc6b-15e9-34277cda6652/mzaf_11558704568117920604.plus.aac.p.m4a"),
         artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/09/01/16/090116af-770e-23da-21a9-6bd30782eda5/00843930013562.rgb.jpg/100x100bb.jpg"),
         trackTimeMillis: 271000
     ),
--- MusicPlayer/Models/MusicTrack.swift
+++ MusicPlayer/Models/MusicTrack.swift
@@ -19,14 +19,19 @@ struct MusicTrack: Identifiable, Codable, Equatable {
         guard let millis = trackTimeMillis else { return nil }
         return TimeInterval(millis) / 1000.0
     }
-    
-    
+
     static let defaultMusicTrack = MusicTrack(
-        id: "1",
+        id: "1755039408",
         name: "Black Friday (pretty like the sun)",
         artist: "Lost Frequencies, Tom Odell, Poppy Baskcomb",
-        previewURL: nil,
-        artworkUrl100: URL(string: "https://lh3.googleusercontent.com/gShVRyvLLbwVB8jeIPghCXgr96wxTHaM4zqfmxIWRsUpMhMn38PwuUU13o1mXQzLMt5HFqX761u8Tgo4L_JG1XLATvw=s0")!,
-        trackTimeMillis: 300000,
+        previewURL: URL(
+            string:
+                "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview211/v4/b2/4a/44/b24a448f-cdb5-f532-6596-753226e0dddc/mzaf_14325479291657353731.plus.aac.p.m4a"
+        ),
+        artworkUrl100: URL(
+            string:
+                "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/39/a7/49/39a749b9-f198-58ea-2198-a214d6c168b7/00198704029172_Cover.jpg/100x100bb.jpg"
+        ),
+        trackTimeMillis: 30000
     )
 }
